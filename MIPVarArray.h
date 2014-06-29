91c99c444e751ef8cfbe1fc7f0998a649032a06f
Igor Machado <igor.machado@gmail.com>
2014-06-29 18:13:21 +0100

    created MIPVarArray4 and MIPVarArray5

--- MIPVarArray.h
+++ MIPVarArray.h
@@ -334,5 +334,228 @@ public:
 };
 
 
+
+class MIPVarArray4
+{
+protected:
+	vector<MIPVarArray3*> v;
+	string name;
+
+public:
+	MIPVarArray4(string _name="") :
+		name(_name)
+	{
+	}
+
+
+	MIPVarArray4(unsigned size, string _name="") :
+		name(_name)
+	{
+		v = vector<MIPVarArray3*>(size, NULL);
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray3;
+	}
+
+	MIPVarArray4(const MIPVarArray4& mva) :
+		name(mva.name), v(mva.v)
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray3(*mva.v[i]);
+	}
+
+	virtual ~MIPVarArray4()
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			delete v[i];
+		v.clear();
+	}
+
+	inline string getName()
+	{
+		return name;
+	}
+
+	inline MIPVarArray4& setName(string _name)
+	{
+		name = _name;
+	}
+
+	void renameVars()
+	{
+		if(name == "")
+			return;
+
+		for(unsigned i=0; i<v.size(); i++)
+			for(unsigned j=0; j<v[i]->size(); j++)
+				for(unsigned k=0; k<(*v[i])[j].size(); k++)
+					for(unsigned l=0; l<(*v[i])[j][k].size(); l++)
+					{
+						stringstream ss;
+						ss << name << "_" << i << "_" << j << "_" << k << "_" << l;
+						(*v[i])[j][k][l].setName(ss.str());
+					}
+	}
+
+	inline MIPVarArray3& operator[](unsigned index)
+	{
+		return *v[index];
+	}
+
+	inline const MIPVarArray3& operator[](unsigned index) const
+	{
+		return *v[index];
+	}
+
+	inline MIPVarArray3& at(unsigned index)
+	{
+		return *v.at(index);
+	}
+
+	inline const MIPVarArray3& at(unsigned index) const
+	{
+		return *v.at(index);
+	}
+
+	inline unsigned size() const
+	{
+		return v.size();
+	}
+
+	inline void push_back(const MIPVarArray3& array)
+	{
+		v.push_back(new MIPVarArray3(array));
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "MIPVarArray4('" << name << "'," << size() << "): " << endl;
+		for(unsigned i=0; i<v.size(); i++)
+			ss << v[i]->toString() << endl;
+		return ss.str();
+	}
+
+	void print() const
+	{
+		cout << toString() << endl;
+	}
+};
+
+
+class MIPVarArray5
+{
+protected:
+	vector<MIPVarArray4*> v;
+	string name;
+
+public:
+	MIPVarArray5(string _name="") :
+		name(_name)
+	{
+	}
+
+
+	MIPVarArray5(unsigned size, string _name="") :
+		name(_name)
+	{
+		v = vector<MIPVarArray4*>(size, NULL);
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray4;
+	}
+
+	MIPVarArray5(const MIPVarArray5& mva) :
+		name(mva.name), v(mva.v)
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray4(*mva.v[i]);
+	}
+
+	virtual ~MIPVarArray5()
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			delete v[i];
+		v.clear();
+	}
+
+	inline string getName()
+	{
+		return name;
+	}
+
+	inline MIPVarArray5& setName(string _name)
+	{
+		name = _name;
+	}
+
+	void renameVars()
+	{
+		if(name == "")
+			return;
+
+		for(unsigned i=0; i<v.size(); i++)
+			for(unsigned j=0; j<v[i]->size(); j++)
+				for(unsigned k=0; k<(*v[i])[j].size(); k++)
+					for(unsigned l=0; l<(*v[i])[j][k].size(); l++)
+						for(unsigned m=0; m<(*v[i])[j][k][m].size(); m++)
+						{
+							stringstream ss;
+							ss << name << "_" << i << "_" << j << "_" << k << "_" << l << "_" << m;
+							(*v[i])[j][k][l][m].setName(ss.str());
+						}
+	}
+
+	inline MIPVarArray4& operator[](unsigned index)
+	{
+		return *v[index];
+	}
+
+	inline const MIPVarArray4& operator[](unsigned index) const
+	{
+		return *v[index];
+	}
+
+	inline MIPVarArray4& at(unsigned index)
+	{
+		return *v.at(index);
+	}
+
+	inline const MIPVarArray4& at(unsigned index) const
+	{
+		return *v.at(index);
+	}
+
+	inline unsigned size() const
+	{
+		return v.size();
+	}
+
+	inline void push_back(const MIPVarArray4& array)
+	{
+		v.push_back(new MIPVarArray4(array));
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "MIPVarArray5('" << name << "'," << size() << "): " << endl;
+		for(unsigned i=0; i<v.size(); i++)
+			ss << v[i]->toString() << endl;
+		return ss.str();
+	}
+
+	void print() const
+	{
+		cout << toString() << endl;
+	}
+};
+
+
+// TODO: generic N-dimensional array
+template<class N>
+class MIPVarArrayN
+{
+
+};
+
 #endif
 
