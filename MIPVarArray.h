d9811cb2260bf9b24c6693f61918a1e745fadd82
Igor Machado <igor.machado@gmail.com>
2014-06-29 18:05:39 +0100

    created MIPVarArray3

--- MIPVarArray.h
+++ MIPVarArray.h
@@ -178,7 +178,7 @@ public:
 			{
 				stringstream ss;
 				ss << name << "_" << i << "_" << j;
-				v[i]->at(j).setName(ss.str());
+				(*v[i])[j].setName(ss.str());
 			}
 	}
 
@@ -218,6 +218,112 @@ public:
 		ss << "MIPVarArray2(" << size() << "): " << endl;
 		for(unsigned i=0; i<v.size(); i++)
 			ss << v[i]->toString() << endl;
+		return ss.str();
+	}
+
+	void print() const
+	{
+		cout << toString() << endl;
+	}
+};
+
+
+class MIPVarArray3
+{
+protected:
+	vector<MIPVarArray2*> v;
+	string name;
+
+public:
+	MIPVarArray3(string _name="") :
+		name(_name)
+	{
+	}
+
+
+	MIPVarArray3(unsigned size, string _name="") :
+		name(_name)
+	{
+		v = vector<MIPVarArray2*>(size, NULL);
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray2;
+	}
+
+	MIPVarArray3(const MIPVarArray3& mva) :
+		name(mva.name), v(mva.v)
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			v[i] = new MIPVarArray2(*mva.v[i]);
+	}
+
+	virtual ~MIPVarArray3()
+	{
+		for(unsigned i=0; i<v.size(); i++)
+			delete v[i];
+		v.clear();
+	}
+
+	inline string getName()
+	{
+		return name;
+	}
+
+	inline MIPVarArray3& setName(string _name)
+	{
+		name = _name;
+	}
+
+	void renameVars()
+	{
+		if(name == "")
+			return;
+
+		for(unsigned i=0; i<v.size(); i++)
+			for(unsigned j=0; j<v[i]->size(); j++)
+				for(unsigned k=0; k<v[i]->at(j).size(); k++)
+				{
+					stringstream ss;
+					ss << name << "_" << i << "_" << j << "_" << k;
+					(*v[i])[j][k].setName(ss.str());
+				}
+	}
+
+	inline MIPVarArray2& operator[](unsigned index)
+	{
+		return *v[index];
+	}
+
+	inline const MIPVarArray2& operator[](unsigned index) const
+	{
+		return *v[index];
+	}
+
+	inline MIPVarArray2& at(unsigned index)
+	{
+		return *v.at(index);
+	}
+
+	inline const MIPVarArray2& at(unsigned index) const
+	{
+		return *v.at(index);
+	}
+
+	inline unsigned size() const
+	{
+		return v.size();
+	}
+
+	inline void push_back(const MIPVarArray2& array)
+	{
+		v.push_back(new MIPVarArray2(array));
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "MIPVarArray3(" << size() << "): " << endl;
+		for(unsigned i=0; i<v.size(); i++)
+			ss << "\t" << v[i]->toString() << endl;
 		return ss.str();
 	}
 
