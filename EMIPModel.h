aff61db687b17e0524807de4f7bff725544d9086
Igor Machado <igor.machado@gmail.com>
2014-07-02 01:35:54 +0100

    created until 5 index parameters

--- EMIPModel.h
+++ EMIPModel.h
@@ -29,7 +29,7 @@ enum ProblemType { Minimize, Maximize };
 
 enum VarType { Real, Binary, Integer }; // (?)
 
-enum Id { IdExpr, IdVar, IdVar1Index, IdVar2Index, IdVar3Index, IdVar4Index, IdVar5Index, IdNum, IdOp, IdComp, IdAnd, IdOr };
+enum Id { IdExpr, IdVar, IdVar1Index, IdVar2Index, IdVar3Index, IdVar4Index, IdVar5Index, IdPar, IdPar1Index, IdPar2Index, IdPar3Index, IdPar4Index, IdPar5Index, IdNum, IdOp, IdComp, IdAnd, IdOr };
 
 class Expr
 {
@@ -345,6 +345,272 @@ public:
 	}
 };
 
+
+class Par : public Expr
+{
+protected:
+	VarType type;
+	string name;
+public:
+
+	Par(string _name, VarType _type = Real) :
+		type(_type), name(_name)
+	{
+	}
+
+	virtual ~Par()
+	{
+	}
+
+	inline string getName() const
+	{
+		return name;
+	}
+
+	inline Par& setName(string _name)
+	{
+		name = _name;
+		return *this;
+	}
+
+	inline bool isInteger() const
+	{
+		return type == Integer;
+	}
+
+	inline bool isBinary() const
+	{
+		return type == Binary;
+	}
+
+	inline bool isReal() const
+	{
+		return type == Real;
+	}
+
+	virtual Id id() const
+	{
+		return IdPar;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+
+		return ss.str();
+	}
+};
+
+class Par1Index : public Par
+{
+protected:
+	Expr i1;
+public:
+
+	Par1Index(string _name, Expr _i1, VarType _type = Real) :
+		Par(_name, _type), i1(_i1)
+	{
+	}
+
+	virtual ~Par1Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdPar1Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar1Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "] ";
+
+		return ss.str();
+	}
+};
+
+class Par2Index : public Par
+{
+protected:
+	Expr i1;
+	Expr i2;
+public:
+
+	Par2Index(string _name, Expr _i1, Expr _i2, VarType _type = Real) :
+		Par(_name, _type), i1(_i1), i2(_i2)
+	{
+	}
+
+	virtual ~Par2Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdPar2Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar2Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "]";
+		ss << "[" << i2.toString() << "] ";
+		return ss.str();
+	}
+};
+
+class Par3Index : public Par
+{
+protected:
+	Expr i1;
+	Expr i2;
+	Expr i3;
+public:
+
+	Par3Index(string _name, Expr _i1, Expr _i2, Expr _i3, VarType _type = Real) :
+		Par(_name, _type), i1(_i1), i2(_i2), i3(_i3)
+	{
+	}
+
+	virtual ~Par3Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdPar3Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar3Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "]";
+		ss << "[" << i2.toString() << "]";
+		ss << "[" << i3.toString() << "] ";
+		return ss.str();
+	}
+};
+
+
+class Par4Index : public Par
+{
+protected:
+	Expr i1;
+	Expr i2;
+	Expr i3;
+	Expr i4;
+public:
+
+	Par4Index(string _name, Expr _i1, Expr _i2, Expr _i3, Expr _i4, VarType _type = Real) :
+		Par(_name, _type), i1(_i1), i2(_i2), i3(_i3), i4(_i4)
+	{
+	}
+
+	virtual ~Par4Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdPar4Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar4Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "]";
+		ss << "[" << i2.toString() << "]";
+		ss << "[" << i3.toString() << "]";
+		ss << "[" << i4.toString() << "] ";
+		return ss.str();
+	}
+};
+
+
+class Par5Index : public Par
+{
+protected:
+	Expr i1;
+	Expr i2;
+	Expr i3;
+	Expr i4;
+	Expr i5;
+public:
+
+	Par5Index(string _name, Expr _i1, Expr _i2, Expr _i3, Expr _i4,  Expr _i5, VarType _type = Real) :
+		Par(_name, _type), i1(_i1), i2(_i2), i3(_i3), i4(_i4), i5(_i5)
+	{
+	}
+
+	virtual ~Par5Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdPar5Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPPar5Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "]";
+		ss << "[" << i2.toString() << "]";
+		ss << "[" << i3.toString() << "]";
+		ss << "[" << i4.toString() << "]";
+		ss << "[" << i5.toString() << "] ";
+		return ss.str();
+	}
+};
 
 class Op : public Expr
 {
