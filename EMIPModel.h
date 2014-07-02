13b7bfe9ff5ff05991fdc1dc849b0b1558df35b7
Igor Machado <igor.machado@gmail.com>
2014-07-02 01:25:57 +0100

    created until EMIP::Or

--- EMIPModel.h
+++ EMIPModel.h
@@ -25,120 +25,290 @@ namespace EMIP
 
 const double Inf = numeric_limits<double>::infinity();
 
-enum MIPType { MIPMinimize, MIPMaximize };
+enum ProblemType { Minimize, Maximize };
 
-enum MIPVarType { MIPReal, MIPBinary, MIPInteger };
+enum VarType { Real, Binary, Integer }; // (?)
 
-class MIPVar
+enum Id { IdExpr, IdVar, IdVar1Index, IdNum, IdOp, IdComp, IdAnd, IdOr };
+
+class Expr
 {
-protected:
-	MIPVarType type;
-	string name;
-
-	double lb;
-	double ub;
-
 public:
 
-	MIPVar(MIPVarType _type = MIPReal, double _lb = 0, double _ub = MIPInf) :
-		type(_type), name(""), lb(_lb), ub(_ub)
+	virtual Id id() const
+	{
+		return IdExpr;
+	}
+
+	virtual string toString() const
+	{
+		return "IdExpr ";
+	}
+
+	virtual void print() const
+	{
+		std::cout << toString() << std::endl;
+	}
+};
+
+class Num : public Expr
+{
+protected:
+	double d;
+public:
+	Num(double _d) :
+		d(_d)
 	{
 	}
 
-	MIPVar(string _name, MIPVarType _type = MIPReal, double _lb = 0, double _ub = MIPInf) :
-		type(_type), name(_name), lb(_lb), ub(_ub)
+	virtual ~Num()
 	{
 	}
 
-	MIPVar(const MIPVar& var) :
-		name(var.name), type(var.type), lb(var.lb), ub(var.ub)
+	virtual Id id() const
+	{
+		return IdNum;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPNum(" << d << ")";
+		return ss.str();
+	}
+};
+
+
+class Var : public Expr
+{
+protected:
+	VarType type;
+	string name;
+public:
+
+	Var(string _name, VarType _type = Real) :
+		type(_type), name(_name)
 	{
 	}
 
-	virtual ~MIPVar()
+	virtual ~Var()
 	{
 	}
-
 
 	inline string getName() const
 	{
 		return name;
 	}
 
-	inline MIPVar& setName(string _name)
+	inline Var& setName(string _name)
 	{
 		name = _name;
 		return *this;
 	}
 
-	inline double getLowerBound() const
-	{
-		return lb;
-	}
-
-	inline MIPVar& setLowerBound(double _lb)
-	{
-		lb = _lb;
-		return *this;
-	}
-
-	inline double getUpperBound() const
-	{
-		return ub;
-	}
-
-	inline MIPVar& setUpperBound(double _ub)
-	{
-		ub = _ub;
-		return *this;
-	}
-
 	inline bool isInteger() const
 	{
-		return type == MIPInteger;
+		return type == Integer;
 	}
 
 	inline bool isBinary() const
 	{
-		return type == MIPBinary;
+		return type == Binary;
 	}
 
 	inline bool isReal() const
 	{
-		return type == MIPReal;
+		return type == Real;
 	}
 
-	void print() const
+	virtual Id id() const
 	{
-		cout << toString() << endl;
+		return IdVar;
 	}
 
 	string toString() const
 	{
 		stringstream ss;
-		ss << "MIPVar(";
+		ss << "EMIPVar(";
 		if(isInteger())
 			ss << "Integer";
 		else if(isBinary())
 			ss << "Binary";
 		else
 			ss << "Real";
-		ss << "):'" << name << "'{" << lb << ";" << ub << "}";
+		ss << ",'" << name << "')";
 
 		return ss.str();
 	}
+};
 
-	MIPVar& operator=(const MIPVar& var)
+class Var1Index : public Var
+{
+protected:
+	Expr i1;
+public:
+
+	Var1Index(string _name, Expr _i1, VarType _type = Real) :
+		Var(_name, _type), i1(_i1)
 	{
-		if(&var == this)
-			return *this;
+	}
 
-		type = var.type;
-		name = var.name;
-		lb   = var.lb;
-		ub   = var.ub;
+	virtual ~Var1Index()
+	{
+	}
 
-		return *this;
+	virtual Id id() const
+	{
+		return IdVar1Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPVar1Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "] ";
+
+		return ss.str();
+	}
+};
+
+class Op : public Expr
+{
+protected:
+	char op;
+	Expr e1;
+	Expr e2;
+public:
+	Op(Expr _e1, char _op, Expr _e2) :
+		e1(_e1), op(_op), e2(_e2)
+	{
+		if((op != '+') && (op != '-') && (op != '*') && (op != '/'))
+		{
+			cout << "ERROR: UNKNOWN OPERATION '" << op << "'" << endl;
+			exit(1);
+		}
+	}
+
+	virtual ~Op()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdOp;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPOp(" << e1.toString() << " " << op << " " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class Comp : public Expr
+{
+protected:
+	string op;
+	Expr e1;
+	Expr e2;
+public:
+
+	// to allow later instantiation of derived classes (strange C++...)
+	Comp()
+	{
+	}
+
+	Comp(Expr _e1, string _op, Expr _e2) :
+		e1(_e1), op(_op), e2(_e2)
+	{
+		if((op != "!=") && (op != "=") && (op != ">") && (op != "<"))
+		{
+			cout << "ERROR: UNKNOWN COMPARISON '" << op << "'" << endl;
+			exit(1);
+		}
+	}
+
+	virtual ~Comp()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdComp;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPComp(" << e1.toString() << " " << op << " " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class And : public Comp
+{
+protected:
+	Comp e1;
+	Comp e2;
+public:
+	And(Comp _e1, Comp _e2) :
+		e1(_e1), e2(_e2)
+	{
+	}
+
+	virtual ~And()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdAnd;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPAnd(" << e1.toString() << " AND " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class Or : public Comp
+{
+protected:
+	Comp e1;
+	Comp e2;
+public:
+	Or(Comp _e1, Comp _e2) :
+		e1(_e1), e2(_e2)
+	{
+	}
+
+	virtual ~Or()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdOr;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPOr(" << e1.toString() << " OR " << e2.toString() << ") ";
+		return ss.str();
 	}
 };
 
--- EMIPModelTest.h
+++ EMIPModelTest.h
@@ -27,22 +27,290 @@ const double Inf = numeric_limits<double>::infinity();
 
 enum ProblemType { Minimize, Maximize };
 
-enum VarType { Real, Binary, Integer };
+enum VarType { Real, Binary, Integer }; // (?)
 
-class Var
+enum Id { IdExpr, IdVar, IdVar1Index, IdNum, IdOp, IdComp, IdAnd, IdOr };
+
+class Expr
+{
+public:
+
+	virtual Id id() const
+	{
+		return IdExpr;
+	}
+
+	virtual string toString() const
+	{
+		return "IdExpr ";
+	}
+
+	virtual void print() const
+	{
+		std::cout << toString() << std::endl;
+	}
+};
+
+class Num : public Expr
+{
+protected:
+	double d;
+public:
+	Num(double _d) :
+		d(_d)
+	{
+	}
+
+	virtual ~Num()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdNum;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPNum(" << d << ")";
+		return ss.str();
+	}
+};
+
+
+class Var : public Expr
 {
 protected:
 	VarType type;
 	string name;
+public:
 
-	double lb;
-	double ub;
-
-	Var(VarType t = EMIP::Real)
+	Var(string _name, VarType _type = Real) :
+		type(_type), name(_name)
 	{
+	}
+
+	virtual ~Var()
+	{
+	}
+
+	inline string getName() const
+	{
+		return name;
+	}
+
+	inline Var& setName(string _name)
+	{
+		name = _name;
+		return *this;
+	}
+
+	inline bool isInteger() const
+	{
+		return type == Integer;
+	}
+
+	inline bool isBinary() const
+	{
+		return type == Binary;
+	}
+
+	inline bool isReal() const
+	{
+		return type == Real;
+	}
+
+	virtual Id id() const
+	{
+		return IdVar;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPVar(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+
+		return ss.str();
 	}
 };
 
+class Var1Index : public Var
+{
+protected:
+	Expr i1;
+public:
+
+	Var1Index(string _name, Expr _i1, VarType _type = Real) :
+		Var(_name, _type), i1(_i1)
+	{
+	}
+
+	virtual ~Var1Index()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdVar1Index;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPVar1Index(";
+		if(isInteger())
+			ss << "Integer";
+		else if(isBinary())
+			ss << "Binary";
+		else
+			ss << "Real";
+		ss << ",'" << name << "')";
+		ss << "[" << i1.toString() << "] ";
+
+		return ss.str();
+	}
+};
+
+class Op : public Expr
+{
+protected:
+	char op;
+	Expr e1;
+	Expr e2;
+public:
+	Op(Expr _e1, char _op, Expr _e2) :
+		e1(_e1), op(_op), e2(_e2)
+	{
+		if((op != '+') && (op != '-') && (op != '*') && (op != '/'))
+		{
+			cout << "ERROR: UNKNOWN OPERATION '" << op << "'" << endl;
+			exit(1);
+		}
+	}
+
+	virtual ~Op()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdOp;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPOp(" << e1.toString() << " " << op << " " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class Comp : public Expr
+{
+protected:
+	string op;
+	Expr e1;
+	Expr e2;
+public:
+
+	// to allow later instantiation of derived classes (strange C++...)
+	Comp()
+	{
+	}
+
+	Comp(Expr _e1, string _op, Expr _e2) :
+		e1(_e1), op(_op), e2(_e2)
+	{
+		if((op != "!=") && (op != "=") && (op != ">") && (op != "<"))
+		{
+			cout << "ERROR: UNKNOWN COMPARISON '" << op << "'" << endl;
+			exit(1);
+		}
+	}
+
+	virtual ~Comp()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdComp;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPComp(" << e1.toString() << " " << op << " " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class And : public Comp
+{
+protected:
+	Comp e1;
+	Comp e2;
+public:
+	And(Comp _e1, Comp _e2) :
+		e1(_e1), e2(_e2)
+	{
+	}
+
+	virtual ~And()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdAnd;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPAnd(" << e1.toString() << " AND " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
+
+
+class Or : public Comp
+{
+protected:
+	Comp e1;
+	Comp e2;
+public:
+	Or(Comp _e1, Comp _e2) :
+		e1(_e1), e2(_e2)
+	{
+	}
+
+	virtual ~Or()
+	{
+	}
+
+	virtual Id id() const
+	{
+		return IdOr;
+	}
+
+	string toString() const
+	{
+		stringstream ss;
+		ss << "EMIPOr(" << e1.toString() << " OR " << e2.toString() << ") ";
+		return ss.str();
+	}
+};
 
 }
 
